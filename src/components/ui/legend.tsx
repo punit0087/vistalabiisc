7e0fbdd62ca9ef321dc657fbb570323d83c3c277
Mohith Kumar <109334910+mohithkumarss@users.noreply.github.com>
2025-03-29 00:17:22 +0530

    Update legend.tsx

--- src/components/ui/legend.tsx
+++ src/components/ui/legend.tsx
@@ -6,19 +6,36 @@ const Legend = ({
   legendItems: { label: string; color: string }[];
 }) => {
   return (
-    <div className="z-[9] fixed right-4 top-[20%] w-[15vw] bg-zinc-800/80 backdrop-blur-sm p-4 rounded-xl text-zinc-300">
+    <div
+      className="z-[9] bg-zinc-800/80 backdrop-blur-sm p-4 rounded-xl text-zinc-300"
+      style={{ width: "18rem" }}
+    >
+      <p className="text-xs font-semibold mb-4 w-full flex flex-col text-center justify-between">
+        <a href="https://mausam.imd.gov.in" target="_blank">
+          India Meteorological Department
+        </a>
+        <a
+          href="https://en.wikipedia.org/wiki/Tropical_cyclone_intensity_scales"
+          target="_blank"
+        >
+          Tropical Cyclone Intensity Scale
+        </a>
+      </p>
       {legendItems.map((item) => (
         <div
           key={item.label}
-          className="flex items-end justify-end space-x-2 space-y-2"
+          className="flex items-center justify-around"
+          style={{ height: "1.5rem" }}
         >
-          {/* Label Text */}
-          <span className="text-zinc-300 text-xs">{item.label}</span>
           {/* Color Box */}
           <div
             className="w-4 h-4 rounded-sm hover:scale-125 transition-all"
             style={{ backgroundColor: item.color }}
           />
+          {/* Label Text */}
+          <span className="text-zinc-300 text-xs ml-2 w-full">
+            {item.label}
+          </span>
         </div>
       ))}
     </div>
