795f8c92c3840315c2be936f6378a177401bb877
Mohith Kumar <ssmohithkumar@gmail.com>
2025-07-07 23:43:44 +0530

    Update tab-data.ts

--- src/app/oppurtunities/tab-data.ts
+++ src/app/oppurtunities/tab-data.ts
@@ -99,8 +99,9 @@ export const tabData: Tab[] = [
         title:
           "Prediction Analysis of Cyclonic Events in Satellite Observations (Cyclonic Observations)",
         designation: "1 Project Associate / Software Engineer",
-        deadlineIso: "2025-05-25T23:59:00+05:30",
-        applyLink: "",
+        deadlineIso: "2025-07-15T23:59:00+05:30",
+        applyLink:
+          "https://docs.google.com/forms/d/e/1FAIpQLScvZo0wBiHqTl2D5tRXl0zj_l_0vDx38aBJNOkdIrnx7MhEeQ/viewform?pli=1",
         content: {
           type: "list",
           items: [
@@ -114,29 +115,12 @@ export const tabData: Tab[] = [
       },
       {
         title:
-          "Development of LLM based Network Traffic Analysis and Cyber Attack Detection (LLM Packet Inspection)",
-        designation: "1 Project Associate / Research Associate",
-        deadlineIso: "2025-05-25T23:59:00+05:30",
-        applyLink: "",
-        content: {
-          type: "list",
-          items: [
-            "B.Tech/MTech/PhD in CS/AI/ECE or related fields with a strong background in machine/deep learning.",
-            "Demonstrated experience of using Large Language Models (LLMs) and natural language processing techniques is required.",
-            "Proficiency in machine learning frameworks (e.g., TensorFlow, PyTorch) is a must.",
-            "Knowledge of networking fundamentals, including protocols (e.g., TCP/IP, HTTP, DNS) and packet inspection concepts.",
-            "Proficiency in Python, with experience in libraries such as Scikit-learn, Numpy, and Pandas.",
-            "Familiarity with tools for network traffic analysis (e.g., Wireshark, tcpdump) and packet data handling is a plus.",
-          ],
-        },
-      },
-      {
-        title:
           "AI/ML and Statistical Analysis for Driving-related Multi-modal Data. (Driving Behaviour Analysis)",
         designation:
           "1 Project Associate / Project Scientist / Research Associate",
-        deadlineIso: "2025-05-25T23:59:00+05:30",
-        applyLink: "",
+        deadlineIso: "2025-07-15T23:59:00+05:30",
+        applyLink:
+          "https://docs.google.com/forms/d/e/1FAIpQLScvZo0wBiHqTl2D5tRXl0zj_l_0vDx38aBJNOkdIrnx7MhEeQ/viewform?pli=1",
         content: {
           type: "list",
           items: [
@@ -145,26 +129,6 @@ export const tabData: Tab[] = [
             "Proven ability to work with and integrate diverse data types such as sensor data,video/image data.",
             "Expertise in Python (with libraries like Pandas, NumPy, scikit-learn)  for data manipulation, statistical analysis, and AI/ML model development.",
             "Domain Knowledge or interest in Driving data analysis.",
-            "Proficiency in machine learning frameworks (e.g., TensorFlow, PyTorch) is a must.",
-          ],
-        },
-      },
-      {
-        title:
-          "Road Infrastructure Assessment using ML/Computer Vision Models on Crowd-Sensing Data Collection through Instrumented Vehicle (Road Computer Vision)",
-        designation: "1 Project Scientist / Software Engineer",
-        deadlineIso: "2025-05-25T23:59:00+05:30",
-        applyLink: "",
-        content: {
-          type: "list",
-          items: [
-            "Proficiency in Python with demonstrated experience in ML and computer vision libraries (e.g., PyTorch, TensorFlow, OpenCV).",
-            "Experience in working with deep learning models for object detection or segmentation (e.g., YOLO, U-Net, Faster R-CNN).",
-            "Familiarity with camera and sensor data integration, including GPS, IMU, and video/image synchronisation.",
-            "Experience in handling real-world data, including pre-processing, cleaning, and annotation.",
-            "Familiarity with deploying models on edge devices (e.g., Jetson Nano/Xavier or Raspberry Pi).",
-            "Understanding of key evaluation metrics (IoU, mAP, precision/recall) and performance bench marking.",
-            "Proficiency in building APIs is a must.",
             "Proficiency in machine learning frameworks (e.g., TensorFlow, PyTorch) is a must.",
           ],
         },
@@ -180,8 +144,9 @@ export const tabData: Tab[] = [
           "AI/ML and Statistical Analysis for Driving-related Multi-modal Data. (Driving Behaviour Analysis)",
         designation:
           "1 Project Associate / Project Scientist / Research Associate",
-        deadlineIso: "2025-05-25T23:59:00+05:30",
-        applyLink: "",
+        deadlineIso: "2025-07-15T23:59:00+05:30",
+        applyLink:
+          "https://docs.google.com/forms/d/e/1FAIpQLScvZo0wBiHqTl2D5tRXl0zj_l_0vDx38aBJNOkdIrnx7MhEeQ/viewform?pli=1",
         content: {
           type: "list",
           items: [
