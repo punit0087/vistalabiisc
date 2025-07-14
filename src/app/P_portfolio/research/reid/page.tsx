1d75f9d25985c54dc160f09aedb4ea94b29b9809
Mohith Kumar <ssmohithkumar@gmail.com>
2025-07-14 14:30:15 +0530

    Update page.tsx

--- src/app/P_portfolio/research/reid/page.tsx
+++ src/app/P_portfolio/research/reid/page.tsx
@@ -10,17 +10,20 @@ export default function Fake_r() {
     <div className="bodyy flex justify-center mt-[8%] sm:mt-[50%]">
       <BackButton />
       <div className="w-[80%] flex flex-col shadow-box p-[60px] sm:p-10">
-        <div className="flex items-center mb-10">
-          <p className="text-2xl font-bold text-zinc-300">Vehicle ReID</p>
-          <p className="text-sm ml-6">Live Interactive Demo: Coming Soon</p>
+        <div className="mb-10">
+          <p className="text-[2.2rem] mb-3 font-bold text-zinc-300">
+            Vehicle ReID
+          </p>
+          <p className="text-xs">Live Interactive Demo: Coming Soon</p>
         </div>
         <iframe
           width="560"
-          height="315"
-          src="https://www.youtube.com/embed/FInVA32gMa4?si=3jPLlN89S7QIgQGq"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        ></iframe>
+          height="600"
+          src="https://www.youtube.com/embed/FInVA32gMa4?autoplay=1&mute=1&loop=1&playlist=FInVA32gMa4&controls=0&modestbranding=1&rel=0"
+          allow="autoplay; encrypted-media; fullscreen"
+          className="rounded-xl border-4 border-zinc-500 w-full"
+        />
+
         {/* <p className="text-sm text-justify p-4">
           The widespread use of global positioning system (GPS) navigation
           systems and wireless communication technology-enabled vehicles have
