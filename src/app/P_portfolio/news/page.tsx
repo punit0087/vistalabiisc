8aa1df8a8b93f5ef451d49897828f896fcbbbee2
Vishwajeet Pattanaik <34767544+vpattanaik@users.noreply.github.com>
2026-07-26 15:28:58 +0200

    fixed typos in news

--- src/app/P_portfolio/news/page.tsx
+++ src/app/P_portfolio/news/page.tsx
@@ -6,46 +6,28 @@ export default function News() {
     <div className="h-[80%] overflow-y-auto scrollbar-custom">
       <ul className="text-xs text-justify animate-blink">
         <li className="mb-4">
-          [Aug 2024] Our interdisciplinary team from IISc, with faculty, students and staff from CDS (lead), CiSTUP, CPS and CDPG, has won the IEEE TCSC SCALE Challenge 2026, held at IEEE/ACM CCGrid 2026 in Sydney <a href="https://www.iisc.ac.in/events/iisc-team-wins-ieee-tcsc-scale-challenge-2026-for-city-scale-real-time-traffic-analytics-across-edge-cloud-fabrics/" target="_blank">Read More</a>
+          [May 2026] Our interdisciplinary team from IISc, with faculty, students and staff from CDS (lead), CiSTUP, CPS and CDPG, has won the IEEE TCSC SCALE Challenge 2026, held at IEEE/ACM CCGrid 2026 in Sydney.
         </li>
         <li className="mb-4">
-          [Aug 2024] Punit Rathore Received Arcot Ramachandran Young Investigator Award from
-           Indian Institute of Science (IISc)
+          [Aug 2024] Punit Rathore Received Arcot Ramachandran Young Investigator Award from Indian Institute of Science (IISc).
         </li>
         <li className="mb-4">
-          [Oct 2023] Punit Rathore will be giving an Invited Talk on "Data-driven Driving
-          Behaviour Analytics – Making the roads safer for all of us!" at "IEEE
-          ITSS: Two-Wheeler Safety and Mobility Technologies" at Ahmedabad
-          University on 4th November 2023
+          [Oct 2023] Punit Rathore will be giving an Invited Talk on "Data-driven Driving Behaviour Analytics – Making the roads safer for all of us!" at "IEEE ITSS: Two-Wheeler Safety and Mobility Technologies" at Ahmedabad University on 4th November 2023.
         </li>
         <li className="mb-4">
-          [Oct 2023] Punit Rathore will be giving an Invited Talk on "Unlocking the Value of
-          Vehicular Data: A Simple, Unsupervised Approach for On-board Driver
-          Behavior Classification" at "AIMLSys 2023: Sustainable AI for Edge
-          workshop, Bangalore" on 28th October 2023
+          [Oct 2023] Punit Rathore will be giving an Invited Talk on "Unlocking the Value of Vehicular Data: A Simple, Unsupervised Approach for On-board Driver Behavior Classification" at "AIMLSys 2023: Sustainable AI for Edge workshop, Bangalore" on 28th October 2023.
         </li>
         <li className="mb-4">
-          [Sep 2023] Punit Rathore will be giving an Invited Talk on "Data-driven Driving
-          Behaviour Analytics – Making the roads safer for all of us!" at
-          Faculty of Industrial Design and Engineering, TU Delft, Netherlands on
-          10th October 2023
+          [Sep 2023] Punit Rathore will be giving an Invited Talk on "Data-driven Driving Behaviour Analytics – Making the roads safer for all of us!" at Faculty of Industrial Design and Engineering, TU Delft, Netherlands on 10th October 2023.
         </li>
         <li className="mb-4">
-          [July 2023] Punit Rathore will be giving an invited talk on "Data-Driven Driving
-          Behaviour Analytics" at Inter-Disciplinary Research Platform (IDRP)
-          IoT Workshop" at IIT Jodhpur on 19th August 2023
+          [July 2023] Punit Rathore will be giving an invited talk on "Data-Driven Driving Behaviour Analytics" at Inter-Disciplinary Research Platform (IDRP) IoT Workshop" at IIT Jodhpur on 19th August 2023.
         </li>
         <li className="mb-4">
-          [Oct 2022] Punit Rathore will be giving an invited talk on "Transportation
-          Revolution through AI: Advance Data Science Approaches to Mobility"
-          for ("Tatsujin Speak") at Mizuho India Japan Study Centre (MIJSC), IIM
-          Bangalore on 4th November 2022
+          [Oct 2022] Punit Rathore will be giving an invited talk on "Transportation Revolution through AI: Advance Data Science Approaches to Mobility" for ("Tatsujin Speak") at Mizuho India Japan Study Centre (MIJSC), IIM Bangalore on 4th November 2022.
         </li>
         <li className="mb-4">
-          [Dec 2021] Our work on "An Unsupervised Approach for Driving Behaviour
-          Analysis of Professional Truck Drivers" has been accepted in EAI
-          International Conference on Intelligent Transportation System
-          (INTSYS), 2021. [Also received the Best Paper Award]
+          [Dec 2021] Our work on "An Unsupervised Approach for Driving Behaviour Analysis of Professional Truck Drivers" has been accepted in EAI International Conference on Intelligent Transportation System (INTSYS), 2021. The work also received the Best Paper Award.
         </li>
       </ul>
     </div>
